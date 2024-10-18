93d76e757e4d8358087396c25c3d5a32b42c7afa
kacem <kacemmeherzi52@gmail.com>
2024-10-18 12:01:56 +0100

    My contact app imported

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'MyContact'
+    'MyContact',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +122,18 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGGING={
+            "version": 1,
+            'handlers': {
+            'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+}
+},
+            'loggers': {
+            'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+}
+}
+}
