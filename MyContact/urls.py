1ede4f3c8b5190fb9066dc788d219af86a4eb726
kacem <kacemmeherzi52@gmail.com>
2024-11-01 10:58:56 +0100

    path to form 2 added

--- MyContact/urls.py
+++ MyContact/urls.py
@@ -1,10 +1,11 @@
 from django.urls import  path
-
 from MyContact import views
 
 
 urlpatterns = [
-   path('add',views.controleform1, name='add'),
+ path('form1/', views.controleform1, name='controleform1'),  
+path('form2/', views.controleform2, name='controleform2'),
+
 
 
 ]
