299cb4fa94b54f2f0f3693b9bc76c6f977bdd75f
kacem <kacemmeherzi52@gmail.com>
2024-11-01 11:12:14 +0100

    mail paths added

--- MyContact/urls.py
+++ MyContact/urls.py
@@ -3,9 +3,10 @@ from MyContact import views
 
 
 urlpatterns = [
- path('form1/', views.controleform1, name='controleform1'),  
+path('form1/', views.controleform1, name='controleform1'),  
 path('form2/', views.controleform2, name='controleform2'),
 
-
+path('contact/', views.contactView, name='contact'),  # URL for the contact form
+path('success/', views.successView, name='success'),  # URL for success page
 
 ]
