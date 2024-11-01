cf905c877ebcfd669592cc13bfe2ce880f48d481
kacem <kacemmeherzi52@gmail.com>
2024-11-01 11:12:03 +0100

    contact form added

--- MyContact/forms.py
+++ MyContact/forms.py
@@ -4,3 +4,11 @@ class contactform2(forms.Form) :
     lastname    = forms.CharField(max_length=10)
     email = forms.EmailField()
     msg = forms.CharField(widget=forms.Textarea)
+
+
+class ContactForm(forms.Form):
+    from_email = forms.EmailField(required=True)
+    subject = forms.CharField(required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
+    to_email=forms.EmailField(required=True)
+    
