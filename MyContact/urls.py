29a7e59d71171cc3489a43ddb7835366d1ddca8a
kacem <kacemmeherzi52@gmail.com>
2024-11-01 09:20:37 +0100

    form1 url added

--- MyContact/urls.py
+++ MyContact/urls.py
@@ -1,8 +1,10 @@
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import  path
+
+from MyContact import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('mycontact/',include('MyConatact.urls')),
+   path('add',views.controleform1, name='add'),
+
 
 ]
