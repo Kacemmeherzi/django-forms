23f54389af194df65171a5f4aebdd220200eeaac
kacem <kacemmeherzi52@gmail.com>
2024-11-01 10:58:12 +0100

    fixed wrong app name

--- django_project/urls.py
+++ django_project/urls.py
@@ -19,6 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mycontact/',include('MyConatact.urls')),
+    path('mycontact/',include('MyContact.urls')),
 
 ]
